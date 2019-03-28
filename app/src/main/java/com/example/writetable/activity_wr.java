f555cf9c48aa1010e3ae2262495c0594461fcdcc
mengmeng liu <lxmshipg@gmail.com>
2019-03-28 05:12:23 -0400

    This is first commit

---
+++ app/src/main/java/com/example/writetable/activity_wr.java
@@ -0,0 +1,13 @@
+package com.example.writetable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_wr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wr);
+    }
+}
