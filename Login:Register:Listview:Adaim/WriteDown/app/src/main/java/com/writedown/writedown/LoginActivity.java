5c3078b8952be16d54ccb802df670ee248d79846
YanglingHu <hylhyl96@gmail.com>
2019-04-14 19:39:10 -0400

    Login/Register/Adaim/Listview

---
+++ Login:Register:Listview:Adaim/WriteDown/app/src/main/java/com/writedown/writedown/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.writedown.writedown;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText mTextUsernameL;
+    EditText mTextPasswordL;
+    Button   mButtonLogin;
+    TextView getViewRegister;
+    dbHelper db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        db = new dbHelper(this);
+        mTextUsernameL = (EditText) findViewById(R.id.login_accountL);
+        mTextPasswordL = (EditText) findViewById(R.id.login_passwordL);
+        mButtonLogin = (Button) findViewById(R.id.login_btn);
+        getViewRegister = (TextView) findViewById(R.id.register);
+        getViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = mTextUsernameL.getText().toString().trim();
+                String pwd = mTextPasswordL.getText().toString().trim();
+
+                boolean res = db.checkUser(user, pwd);
+
+                if(!(user.length()>=6&&user.length()<=20)) {
+                    Toast.makeText(LoginActivity.this, "Username should not less than 6", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!(pwd.length()>0)) {
+                    Toast.makeText(LoginActivity.this, "Enter the password", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                if (res == true) {
+                    Intent moveToMain = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(moveToMain);
+                }
+
+                if(user.equals("123321")&&pwd.equals("123321")) {
+                    Intent moveToAdam = new Intent(LoginActivity.this, AdamActivity.class);
+                    startActivity(moveToAdam);
+                }
+                if(res ==false) {
+                    Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
+
